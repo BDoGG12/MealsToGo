7d74da49885a6e2c5ebde1e603747f54286db9da
Ben Do <bdo.appworkshop@gmail.com>
2022-09-10 10:13:00 -0500

    Created List component

---
+++ src/components/List.js
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+const List = (props) => {
+
+  return (
+    <View style={{
+      flex: 1,
+      padding: 16,
+      backgroundColor: 'blue',
+    }}>
+      <Text>list</Text>
+    </View>
+  );
+};
+
+export default List;
