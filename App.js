326c4d355e738d847a793dd97672e535ee3a5f15
Ben Do <bdo.appworkshop@gmail.com>
2022-09-10 10:19:07 -0500

    Added Search, List, and SafeAreaView to App.

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+import Search from './src/components/Search.js';
+import List from './src/components/List.js';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView
+        style={
+          {
+            flex: 1,
+          }
+        }
+      >
+        <Search text='search' />
+        <List text='list' />
+      </SafeAreaView>
+    </>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
 });
